ac2a2edef72d06271ba442b16f838f26da038f1f
Janik Andreas <j.andreas@posteo.de>
2018-10-30 16:35:35 -0700

    Summary of Work: * Setting up file structure, with components and styles folder.
    Known Issues:
    * None.

---
+++ src/components/BottomNavbar.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../styles/BottomNavbar.scss';
+
+class BottomNavbar extends Component {
+  render() {
+    return (
+      <div className='bottom-navbar'>BottomNavBar</div>
+    );
+  }
+}
+
+export default BottomNavbar;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../styles/Navbar.scss';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className='navbar'>NavBar</div>
+    );
+  }
+}
+
+export default Navbar;
